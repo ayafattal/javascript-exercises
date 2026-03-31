8dc0a123b09f6cbb81e22daed8aa77f64f659cde
Aya fattal <120955394+ayafattal@users.noreply.github.com>
2026-03-31 21:47:27 +0300

    Update joinStrings.js

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -12,9 +12,10 @@ const firstName = "Carlos";
 const lastName = "Steven";
 const thisYear = 1965;
 const birthYear = 1947;
-const fullName = "Carlos Steven";
-const age = "18"
 
+
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 const greeting = " Hello! My name is " + fullName +  " and i am " + age + " years old.";
 
 console.log(greeting);
